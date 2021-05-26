7cc5e947143bb04984cae32f1cd38d1342e9e7f7
SyandaMthwethwa <83707630+SyandaMthwethwa@users.noreply.github.com>
2021-05-26 14:41:33 +0200

    Commit 4

---
+++ RTIS_Vulcan_MBL/obj/Debug/51/android/src/mono/MonoPackageManager_Resources.java
@@ -0,0 +1,9 @@
+package mono;
+public class MonoPackageManager_Resources {
+	public static String[] Assemblies = new String[]{
+		/* We need to ensure that "RTIS_Vulcan_MBL.dll" comes first in this list. */
+		"RTIS_Vulcan_MBL.dll",
+	};
+	public static String[] Dependencies = new String[]{
+	};
+}
